036b993dd4d51f05b9433cb4e2e1c96304f4e85d
Yun Feng <yun.feng@smartx.com>
2021-04-21 18:59:55 +0800

    build: remove .js.map files

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ cp -r public ../storyteller-website/
 cd ../storyteller-website && \
 rm -rf public/smoothie.html && \
 rm -rf public/snap-flow.html && \
+rm -rf public/build/*.map && \
 mv public/storyteller.html public/index.html
